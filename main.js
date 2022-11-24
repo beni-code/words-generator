eb7613ace8f7fad672eee4734128649195d07113
beni-code <53158397+beni-code@users.noreply.github.com>
2022-11-24 07:23:03 +0000

    Deploying to gh-pages from  @ 39f5c866ecd4117610cf3c701603a363a4b459e8 🚀

--- main.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const parseCsv = require('./parseCsv')
-
-fs.copyFileSync("./index.html", './dist/index.html')
-fs.copyFileSync("./vue.js", './dist/vue.js')
-
-parseCsv().then(res => {
-    fs.writeFileSync("./dist/index.js", `let list = ${JSON.stringify(res)}`, 'utf8')
-})
