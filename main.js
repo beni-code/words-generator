b5b39aed47b700e061f98e9aa783d29424649063
ben.li <53158397+beni-code@users.noreply.github.com>
2022-11-24 15:19:38 +0800

    1

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 const parseCsv = require('./parseCsv')
 
-fs.copyFileSync("./index.html",'./dist/index.html')
-fs.copyFileSync("./vue.js",'./dist/vue.js')
+if (!fs.statSync('./dist').isDirectory()) {
+    fs.mkdirSync("./dist")
+}
+fs.copyFileSync("./index.html", './dist/index.html')
+fs.copyFileSync("./vue.js", './dist/vue.js')
 
-parseCsv().then(res=>{
-    fs.writeFileSync("./dist/index.js",`let list = ${JSON.stringify(res)}`,'utf8')
+parseCsv().then(res => {
+    fs.writeFileSync("./dist/index.js", `let list = ${JSON.stringify(res)}`, 'utf8')
 })
