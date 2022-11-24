3cbf47abdbc7e85c1bbedc6b15462c91875bd495
ben.li <53158397+beni-code@users.noreply.github.com>
2022-11-24 15:18:12 +0800

    1

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const parseCsv = require('./parseCsv')
 
+fs.copyFileSync("./index.html",'./dist/index.html')
+fs.copyFileSync("./vue.js",'./dist/vue.js')
+
 parseCsv().then(res=>{
-    fs.writeFileSync("./index.js",`let list = ${JSON.stringify(res)}`,'utf8')
+    fs.writeFileSync("./dist/index.js",`let list = ${JSON.stringify(res)}`,'utf8')
 })
