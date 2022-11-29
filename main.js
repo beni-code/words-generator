cbf96386998e21882d5b80d2e231272ce3c3fd74
ben.li <53158397+beni-code@users.noreply.github.com>
2022-11-29 15:00:42 +0800

    1

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const fs = require('fs')
 const parseCsv = require('./parseCsv')
 
+try{
+    fs.mkdirSync("./dist")
+}catch(err){
+    console.log("dist exist");
+}
+
+
 fs.copyFileSync("./index.html", './dist/index.html')
 fs.copyFileSync("./convertor.html", './dist/convertor.html')
 fs.copyFileSync("./random.svg", './dist/random.svg')
